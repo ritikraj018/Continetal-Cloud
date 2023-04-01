9f0fc52eb1f41969f33e339c321429639177ea74
Ritik Raj <ritikrajbarh18@gmail.com>
2023-04-01 10:17:25 +0530

    final link

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ var express = require('express'), // "^4.13.4"
     multerS3 = require('multer-s3'); //"^1.4.1"
 
 aws.config.update({
-    secretAccessKey: 'MQKlgl9Xu3Dvl4wvv4a/TeOBpXMGrSKHFNQjOxgx',
-    accessKeyId: 'AKIATICNI2SOQHY3GO4W',
-    region: 'us-east-1'
+    secretAccessKey: process.env.A_SKEYID,
+    accessKeyId: process.env.A_KEYID,
+    region: process.env.REGION
 });
 
-
+console.log(process.env.API_KEY);
 
 var app = express(),
     s3 = new aws.S3();
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //connect db
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/userDB1", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -52,10 +52,6 @@ app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get('/index', function (req, res) {
-    res.render("\index");
-});
-
 app.get('/login', function (req, res) {
     res.render("login")
 });
@@ -69,7 +65,7 @@ app.post('/upload', upload.array('upl', 25), function (req, res, next) {
     res.send({
         message: "Uploaded!",
         urls: req.files.map(function (file) {
-            return { url: file.location, name: file.key, type: file.mimetype, size: file.size };
+            res.render("load");
         })
     });
 });
@@ -79,7 +75,6 @@ app.post("/signup", function (req, res) {
         email: req.body.username,
         password: req.body.password
     });
-
     newUser.save()
         .then(() => {
             // Do something else
@@ -94,12 +89,14 @@ app.post("/signup", function (req, res) {
 app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
-
     User.findOne({ email: username })
         .exec()
         .then(foundUser => {
             if (foundUser && foundUser.password === password) {
                 res.render("index");
+            }
+            else {
+                res.render("wrong");
             }
         })
         .catch(err => {
